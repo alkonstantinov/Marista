cdc0d2f3fdff1bfbdda8d3a45ac9307a75de3b0a
Venko Andonov <vandonov@unwe.bg>
2017-12-22 17:29:32 +0200

    labels

--- Marista.Common/ViewModels/ProductVM.cs
+++ Marista.Common/ViewModels/ProductVM.cs
@@ -10,20 +10,36 @@ namespace Marista.Common.ViewModels
     public class ProductVM
     {
         public int ProductId { get; set; }
+
         [Required, MaxLength(100)]
+        [Display(Name="Name")]
         public string Name { get; set; }
+
         [Required]
+        [Display(Name = "Description")]
         public string Description { get; set; }
+
         [Required]
+        [Display(Name = "Price")]
         public decimal Price { get; set; }
+
+        [Display(Name = "Promotional price")]
         public Nullable<decimal> PromotionalPrice { get; set; }
+
         [Required]
+        [Display(Name = "Vertical category")]
         public int VCategoryId { get; set; }
+
         public string VCategoryCategoryName { get; set; }
+
         [Required]
+        [Display(Name = "Horizontal category")]
         public int HCategoryId { get; set; }
+
         public string HCategoryCategoryName { get; set; }
+
         //required, enforced by the controller
+        [Display(Name = "Picture")]
         public byte[] Picture { get; set; }
     }
 }
